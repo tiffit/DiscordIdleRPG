ab2a7296645d5e9feb32d757ebf72909a385193a
tiffit <ttiffit@yahoo.com>
2019-06-28 10:00:41 -0700

    Added backpack drop command

--- commands/backpack.js
+++ commands/backpack.js
@@ -1,5 +1,6 @@
 const db = require("./../database");
 const itemloader = require('./../items');
+const main = require("./../main");
 const util = require('../util');
 
 exports.run = async function (discord, bot, args, member, channel) {
@@ -8,13 +9,82 @@ exports.run = async function (discord, bot, args, member, channel) {
             channel.send(util.noAccountMessage());
             return;
         }
-
+        if(args.length > 0){
+            if(args[0].toLowerCase() === "drop"){
+                var usage = `Usage: ${main.properties.prefix}backpack drop <amount> <item name>`;
+                if(args.length < 3){
+                    channel.send(usage);
+                    return;
+                }
+                if(!/^\d+$/.test(args[1])){
+                    channel.send(usage);
+                    return;
+                }
+                var amount = parseInt(args[1]);
+                var name = args.slice(2).join(" ");
+                var item = itemloader.fromName(name);
+                if(!item || !data.backpack[item.internal]){
+                    channel.send(name + " is not a valid item in your backpack!");
+                    return;
+                }
+                var holding = data.backpack[item.internal];
+                var new_holing = holding - amount;
+                if(new_holing < 0){
+                    let embed = new discord.RichEmbed()
+                        .setTimestamp()
+                        .setDescription(`You do not have enough ${item.name}!`)
+                        .setAuthor("Backpack", bot.user.displayAvatarURL)
+                        .setTitle("Drop Error")
+                        .setFooter(member.displayName, member.user.avatarURL)
+                        .setColor([168, 15, 15])
+                        .addField("Current Count", holding, true)
+                        .addField("Amount Dropping", amount, true);
+                        channel.send(embed);
+                    return;
+                }
+                let embed = new discord.RichEmbed()
+                    .setTimestamp()
+                    .setDescription(`✅ to confirm and ❌ to cancel. This will expire in 1 minute.`)
+                    .setAuthor("Backpack", bot.user.displayAvatarURL)
+                    .setTitle("Drop Confirmation")
+                    .setFooter(member.displayName, member.user.avatarURL)
+                    .addField("Item", item.name, true)
+                    .addField("Count", amount, true);
+                    channel.send(embed).then(async (msg) => {
+                        await msg.react('✅');
+                        await msg.react('❌');
+                        const filter = (reaction, user) => reaction.emoji.name === '✅' || '❌' && user.id === member.user.id;
+                        const collector = msg.createReactionCollector(filter, { time: 60000 });
+                        collector.on('collect', (r) => {
+                            msg.clearReactions();
+                            if(r.emoji.name === '✅'){
+                                embed = embed.setTimestamp()
+                                    .setDescription(`Confirmed! ✅`)
+                                    .setColor([15, 168, 15]);
+                                msg.edit(embed);
+                                util.removeItem(data.backpack, item, amount);
+                                db.updateUserObj(data);
+                            }
+                            if(r.emoji.name === '❌'){
+                                embed = embed.setTimestamp()
+                                    .setDescription(`Canceled! ❌`)
+                                    .setColor([168, 15, 15]);
+                                msg.edit(embed);
+                            }
+                            collector.stop();
+                        });
+                });
+                return;
+            }
+        }
+        var description = `\`${main.properties.prefix}backpack drop <amount> <item name>\` to drop items from your backpack.\n`;
+        description += "React with 👍 to move all items from the backpack to the inventory.\n";
+        description += "Storage: " + util.getTotalCount(data.backpack) + "/" + util.getBackpackStorage(data);
         const embed = new discord.RichEmbed()
             .setTimestamp()
             .setColor([24, 224, 200])
             .setAuthor("Backpack", bot.user.displayAvatarURL)
-            .setDescription("Storage: " + util.getTotalCount(data.backpack) + "/" + util.getBackpackStorage(data) + "\nReact with 👍 to move all items from the backpack to the inventory.")
-            
+            .setDescription(description)
             .setFooter(`${member.displayName}`, member.user.avatarURL);
         var invStr = "";
         for (let [internal, count] of Object.entries(data.backpack)) {
